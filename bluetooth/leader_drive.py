7984bafdcc0cfde4d2f0fe76c8e8f06873576b7e
James Rolfe <jamesnrolfe@gmail.com>
2019-11-10 23:37:27 -0500

    restructure and bug fix

--- bluetooth/leader_drive.py
+++ bluetooth/leader_drive.py
@@ -2,13 +2,12 @@ from recv_from import RecvFromAgent
 from SWARMER_BT_INFO import UUID
 
 from motor.motor_driver import MotorDriver
-from motor.MOTOR_CONFIG import THROTTLE, RUN_TIME
-
+from motor.MOTOR_CONFIG import THROTTLE, RUN_TIME, MSG_SIZE
 
 md = MotorDriver(THROTTLE, RUN_TIME)
 
 try:
-    recv_agent = RecvFromAgent(UUID)
+    recv_agent = RecvFromAgent(UUID, MSG_SIZE)
     recv_agent.advertise()
     while True:
         receivedData = recv_agent.read()
