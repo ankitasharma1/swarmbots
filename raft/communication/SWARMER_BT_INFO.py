ac7c31d90a0e6cda9bad66f4a2dad96f67c163ee
james <jamesnrolfe@gmail.com>
2019-11-23 19:18:29 -0500

    update bt_info_dict

--- raft/communication/SWARMER_BT_INFO.py
+++ raft/communication/SWARMER_BT_INFO.py
@@ -1,12 +1,22 @@
 SWARMER_ID_DICT = {
-    "S1": 'B8:27:EB:19:6A:75',
-    "S2": 'B8:27:EB:6A:D3:2D',
-    "S3": 'B8:27:EB:0D:A8:B9'
+    "S1": {
+        "ADDR": "B8:27:EB:19:6A:75",
+        "PORT": 136
+    },
+    "S2": {
+        "ADDR": "B8:27:EB:6A:D3:2D",
+        "PORT": 137
+    },
+    "S3": {
+        "ADDR": "B8:27:EB:0D:A8:B9",
+        "PORT": 138
+    }
 }
+
 SWARMER_ADDR_DICT = {
-    'B8:27:EB:19:6A:75': 'S1',
-    'B8:27:EB:6A:D3:2D': 'S2',
-    'B8:27:EB:0D:A8:B9': 'S3'
+    "B8:27:EB:19:6A:75": "S1",
+    "B8:27:EB:6A:D3:2D": "S2",
+    "B8:27:EB:0D:A8:B9": "S3"
 }
 
 UUID = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
