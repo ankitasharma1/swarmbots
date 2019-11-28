4d24f39b6f30545bfebc620d8bd2c6ec64df6e88
James Rolfe <jamesnrolfe@gmail.com>
2019-11-27 19:55:24 -0500

    testing bt raft

--- raft/main.py
+++ raft/main.py
@@ -9,7 +9,7 @@ if len(sys.argv) != 2:
 # Unique node id.
 node_id = sys.argv[1]
 
-n = Node(node_id, wifi=True, debug=True)
+n = Node(node_id, wifi=False, debug=True)
 
 # Node 'init' routine.
 n.init()
