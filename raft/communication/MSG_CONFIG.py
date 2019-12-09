ce120d24ef7c066edc0c61d247f542e32782e6e8
James Rolfe <jamesnrolfe@gmail.com>
2019-12-09 02:07:08 -0500

    becomes unstable around 1500, try again

--- raft/communication/MSG_CONFIG.py
+++ raft/communication/MSG_CONFIG.py
@@ -4,11 +4,11 @@ PADDING_BYTE = b' '
 
 # socket config, this is how long the recv function will wait while checking
 # for messages
-RECV_TIMEOUT = 0.2  # seconds
+RECV_TIMEOUT = 0.15  # seconds
 
 # socket recv and send delay; we run into bandwidth issues over BT if we send
 # and check recv as fast as we can
-MSG_SEND_DELAY = RECV_TIMEOUT
+MSG_SEND_DELAY = RECV_TIMEOUT + 0.05
 MSG_RECV_DELAY = RECV_TIMEOUT
 
 # message types
