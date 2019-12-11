0523f70de2fbdc2d2bf7278c053ef2e310a90f74
Ankita Sharma <asharma7@cs.brown.edu>
2019-12-10 23:53:44 -0500

    follower code

--- raft/follower_drive.py
+++ raft/follower_drive.py
@@ -24,26 +24,45 @@ class FollowerDriving:
 
     def _drive(self):
         while self.on:
-            res = self.swarmer_cam.pollCameraForBot()
-            x_coord = res[0]
-            y_coord = res[1]
-            should_drive = res[2]
+            res = None
+            i = 0
+            # Poll for swarm a few times before making any decisions.
+            while i < 3:
+                res = self.swarmer_cam.pollCameraForBot()
+                # If we detect the swarm, break.
+                if res:
+                    break
+                i += 1
+            
+            # We are oriented towards the swarm.
+            if res:
+                x_coord = res[0]
+                y_coord = res[1]
+                should_drive = res[2]
 
-            # Need to drive.
-            if should_drive:
-                # Orient.
-                i = 0
-                while i < 3:
-                    res = self.swarmer_cam.pollCameraForBot()
-                    # Left
-                    if x_coord < 0:
-                        self.motor.orient_left()
-                    # Right
-                    else:
-                        self.motor.orient_right()
-                    i += 1
-            time.sleep(5)
-            self.motor.stop()
+                # We are far away from something in view.
+                if should_drive:
+                    # Orient.
+                    j = 0
+                    while j < 3:
+                        res = self.swarmer_cam.pollCameraForBot()
+                        # Left
+                        if x_coord < 0:
+                            self.motor.orient_left()
+                        # Right
+                        else:
+                            self.motor.orient_right()
+                        j += 1
+                    time.sleep(5)
+                    self.motor.stop()
+                # We are close to something in view.
+                else:
+                    # Do nothing.
+                    pass
+            # We are not oriented towards the swarm.
+            else:
+                # Orient to the right and see if anything changes.
+                self.motor.orient_right()
     
     def stop(self, testing=False):
         # testing allows stopping and starting without killing the conn
