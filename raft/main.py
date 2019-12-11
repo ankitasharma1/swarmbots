7d369b3388c66da2b0e40c91a0d302a78f28c045
James Rolfe <jamesnrolfe@gmail.com>
2019-12-11 01:13:30 -0500

    comment out calibration for testing

--- raft/main.py
+++ raft/main.py
@@ -3,10 +3,11 @@ from node import Node
 from communication.SWARMER_ID import SWARMER_ID
 from calibrate_rssi import RssiCalibrator
 
+# TODO: uncomment for presentation
 # Callibrate per the room.
-r = RssiCalibrator()
-r.calibrate()
-r.store_rssi()
+# r = RssiCalibrator()
+# r.calibrate()
+# r.store_rssi()
 
 # Create the node object.
 n = Node(SWARMER_ID, wifi=False, debug=False)
